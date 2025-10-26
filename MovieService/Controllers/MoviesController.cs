22a97aeb2f9ddfaed4b914492ec63c1521667743
TalalBnoob <79761207+TalalBalnoob@users.noreply.github.com>
2025-10-26 14:23:52 +0300

    [*] add JWT auth to all serivecs along with the JWT Key(For Dev Only)

--- MovieService/Controllers/MoviesController.cs
+++ MovieService/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 using MovieService.data;
 using Microsoft.AspNetCore.Mvc;
 using MovieService.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MovieService.Controllers;
 
@@ -8,16 +9,19 @@ namespace MovieService.Controllers;
 [ApiController]
 public class MoviesController(AppDbContext db) : ControllerBase {
 	[HttpGet("health")]
+	[AllowAnonymous]
 	public IActionResult Health() {
 		return Ok("Movie Service is healthy");
 	}
 	[HttpGet("/")]
+	[AllowAnonymous]
 	public IActionResult GetAll() {
 		var moviesList = db.Movies.ToList();
 		return Ok(moviesList);
 	}
 
 	[HttpGet("/{id}")]
+	[AllowAnonymous]
 	public IActionResult GetById(int id) {
 		var movie = db.Movies.Find(id);
 		if (movie == null) return NotFound();
@@ -25,6 +29,7 @@ public class MoviesController(AppDbContext db) : ControllerBase {
 	}
 
 	[HttpPost("/")]
+	[Authorize]
 	public IActionResult CreateMovie(Movies movie) {
 		var newMovie = new Movies {
 			Id = 0,
@@ -40,6 +45,7 @@ public class MoviesController(AppDbContext db) : ControllerBase {
 
 	}
 
+	[Authorize]
 	[HttpPut("/{id}")]
 	public IActionResult UpdateMovie(int id, Movies updatedMovie) {
 		var existingMovie = db.Movies.Find(id);
@@ -54,6 +60,7 @@ public class MoviesController(AppDbContext db) : ControllerBase {
 		return NoContent();
 	}
 
+	[Authorize]
 	[HttpDelete("/{id}")]
 	public IActionResult DeleteMovie(int id) {
 		var movie = db.Movies.Find(id);
--- MovieService/Program.cs
+++ MovieService/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 using MovieService.data;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 		);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+		.AddJwtBearer(options => {
+			options.TokenValidationParameters = new TokenValidationParameters {
+				ValidateIssuer = true,
+				ValidateAudience = true,
+				ValidateLifetime = true,
+				ValidateIssuerSigningKey = true,
+				ValidIssuer = builder.Configuration["Jwt:Issuer"],
+				ValidAudience = builder.Configuration["Jwt:Audience"],
+				IssuerSigningKey = new SymmetricSecurityKey(
+							Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+			};
+		});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +41,7 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
