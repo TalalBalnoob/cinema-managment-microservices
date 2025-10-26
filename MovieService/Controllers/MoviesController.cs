251aa8483122674390fcbab3e967ee528a8800ed
TalalBnoob <79761207+TalalBalnoob@users.noreply.github.com>
2025-10-26 11:39:56 +0300

    [MovieService] complete CRUD in the movies controller

--- MovieService/Controllers/MoviesController.cs
+++ MovieService/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 using MovieService.data;
 using Microsoft.AspNetCore.Mvc;
+using MovieService.Models;
 
 namespace MovieService.Controllers;
 
@@ -14,5 +15,53 @@ public class MoviesController(AppDbContext db) : ControllerBase {
 	public IActionResult GetAll() {
 		var moviesList = db.Movies.ToList();
 		return Ok(moviesList);
+	}
+
+	[HttpGet("/{id}")]
+	public IActionResult GetById(int id) {
+		var movie = db.Movies.Find(id);
+		if (movie == null) return NotFound();
+		return Ok(movie);
+	}
+
+	[HttpPost("/")]
+	public IActionResult CreateMovie(Movies movie) {
+		var newMovie = new Movies {
+			Id = 0,
+			Title = movie.Title,
+			Description = movie.Description ?? string.Empty,
+			Duration = movie.Duration,
+			Rating = movie.Rating
+		};
+
+		db.Movies.Add(newMovie);
+		db.SaveChangesAsync();
+		return CreatedAtAction(nameof(GetAll), new { id = newMovie.Id }, newMovie);
+
+	}
+
+	[HttpPut("/{id}")]
+	public IActionResult UpdateMovie(int id, Movies updatedMovie) {
+		var existingMovie = db.Movies.Find(id);
+		if (existingMovie == null) return NotFound();
+
+		existingMovie.Title = updatedMovie.Title;
+		existingMovie.Description = updatedMovie.Description;
+		existingMovie.Duration = updatedMovie.Duration;
+		existingMovie.Rating = updatedMovie.Rating;
+
+		db.SaveChangesAsync();
+		return NoContent();
+	}
+
+	[HttpDelete("/{id}")]
+	public IActionResult DeleteMovie(int id) {
+		var movie = db.Movies.Find(id);
+		if (movie == null) {
+			return NotFound();
+		}
+		db.Movies.Remove(movie);
+		db.SaveChangesAsync();
+		return NoContent();
 	}
 }
