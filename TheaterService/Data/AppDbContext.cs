22115c1d22003d2cc9d10f96cac6bccb88038141
TalalBnoob <79761207+TalalBalnoob@users.noreply.github.com>
2025-10-28 13:27:55 +0300

    [TheaterService] add migrations and theater controller

--- TheaterService/Data/AppDbContext.cs
+++ TheaterService/Data/AppDbContext.cs
@@ -2,8 +2,16 @@ using System;
 
 using Microsoft.EntityFrameworkCore;
 
+using TheaterService.Models;
+
 namespace TheaterService.Data;
 
 public class AppDbContext : DbContext {
-	AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+	public DbSet<Theater> Theaters { get; set; }
+	public DbSet<Seat> Seats { get; set; }
+	public DbSet<Hall> Halls { get; set; }
+	public DbSet<Showtime> Showtimes { get; set; }
+	public DbSet<ShowTimeSeat> ShowTimeSeats { get; set; }
 }
