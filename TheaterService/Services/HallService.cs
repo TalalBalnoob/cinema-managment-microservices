7f1e215870f24ffb852d37bda3306df952077dec
TalalBalnoob <TalalBalnoob@gmail.com>
2025-11-09 13:45:49 +0300

    [Theater] add the seatSerives

--- TheaterService/Services/HallService.cs
+++ TheaterService/Services/HallService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +9,7 @@ using TheaterService.Models;
 
 namespace TheaterService.Services;
 
-public class HallService(AppDbContext db) {
+public class HallService(AppDbContext db, SeatService seatService) {
 	public async Task<ICollection<Hall>> GetAllHalls(int theater_id) {
 		var halls = db.Halls
 			.Include(h => h.Seats)
@@ -51,27 +52,26 @@ public class HallService(AppDbContext db) {
 		return hall;
 	}
 
-	public void DeleteHall(int theater_id, int id) {
+	public async Task DeleteHall(int theater_id, int id) {
 		var hall = db.Halls.FirstOrDefault(h => h.Id == id && h.Theater_Id == theater_id);
 		if (hall == null) throw new Exception("Hall not found");
 
 		var showsInHall = db.Showtimes.Any(s => s.Hall_Id == id);
 		if (showsInHall) throw new Exception("Cannot delete hall with scheduled showtimes");
 
-		var seats = db.Seats.Where(s => s.Hall_Id == id);
+		seatService.DeleteAllSeat(id);
 
-		db.Seats.RemoveRange(seats);
 		db.Halls.Remove(hall);
 		db.SaveChanges();
 		return;
 	}
 
-	public void DeleteAllHalls(int theater_id) {
+	public async Task DeleteAllHalls(int theater_id) {
 		var halls = db.Halls.Where(h => h.Theater_Id == theater_id);
 		if (halls == null) return;
 
 		foreach (var hall in halls) {
-			DeleteHall(theater_id, hall.Id);
+			await DeleteHall(theater_id, hall.Id);
 		}
 
 		return;
---
+++ TheaterService/Services/SeatService.cs
@@ -0,0 +1,93 @@
+using System;
+
+using Microsoft.EntityFrameworkCore;
+
+using TheaterService.Data;
+using TheaterService.DTOs;
+using TheaterService.Migrations;
+using TheaterService.Models;
+
+namespace TheaterService.Services;
+
+public class SeatService(AppDbContext db) {
+	public async Task<IEnumerable<Seat>> GetAllSeats(int hall_id) {
+		var seats = db.Seats.Where(s => s.Hall_Id == hall_id);
+
+		return seats;
+	}
+
+	public async Task<Seat?> GetSeat(int id) {
+		var seat = db.Seats.Find(id);
+
+		return seat;
+	}
+
+	public async Task<IEnumerable<Seat>> GetSeats(List<int> ids) {
+		var seats = await db.Seats.Where(s => ids.Contains(s.Id)).ToListAsync();
+
+		return seats;
+	}
+
+	public async Task<Seat> CreateSeat(int hall_id, NewSeat newSeat) {
+		var hall = db.Halls.Find(hall_id);
+		if (hall == null) throw new Exception("Hall not found!");
+
+		if (newSeat.Number >= hall.Layout_columns) {
+			throw new Exception($"Seat number must be less than the number of columns in the hall ({hall.Layout_columns}).");
+		}
+
+		if (newSeat.Row >= hall.Layout_rows) {
+			throw new Exception($"Seat row number must be less than the number of rows in the hall ({hall.Layout_rows}).");
+		}
+
+		var seat = db.Seats.Add(new Seat {
+			Id = 0,
+			Number = newSeat.Number,
+			Row = newSeat.Row,
+			Hall_Id = hall_id,
+			IsActive = newSeat.IsActive,
+			Seat_type = newSeat.Seat_type,
+		});
+
+		db.SaveChanges();
+		return seat.Entity;
+	}
+
+	public async Task<Seat?> UpdateSeat(int hall_id, int id, NewSeat newSeat) {
+		var seat = db.Seats.Include(s => s.Hall).Single(s => s.Id == id || s.Hall_Id == hall_id);
+		if (seat == null) throw new Exception("Seat not found!");
+		if (seat.Hall == null) throw new Exception("Hall not found!");
+
+
+		if (newSeat.Number >= seat.Hall.Layout_columns) {
+			throw new Exception($"Seat number must be less than the number of columns in the hall ({seat.Hall.Layout_columns}).");
+		}
+
+		if (newSeat.Row >= seat.Hall.Layout_rows) {
+			throw new Exception($"Seat row number must be less than the number of rows in the hall ({seat.Hall.Layout_rows}).");
+		}
+
+		seat.Number = newSeat.Number;
+		seat.Row = newSeat.Row;
+		seat.IsActive = newSeat.IsActive;
+		seat.Seat_type = newSeat.Seat_type;
+
+		db.SaveChanges();
+
+		return seat;
+	}
+
+	public void DeleteSeat(int hall_id, int id) {
+		var seat = db.Seats.Find(id);
+		if (seat == null || seat.Hall_Id != hall_id) throw new Exception("Seat not found");
+
+		db.Seats.Remove(seat);
+		db.SaveChanges();
+	}
+
+	public async void DeleteAllSeat(int hall_id) {
+		var seats = db.Seats.Where(s => s.Hall_Id == hall_id);
+
+		db.Seats.RemoveRange(seats);
+	}
+}
